c92176392ad33aed0f7d1ca9e54c01bd9127a6c4
Rista Khadka <ristakhadka@Ristas-MacBook-Pro.local>
2026-08-11 13:41:40 +0545

    Analysis completed

---
+++ sql/Queries_and_analysis.sql
@@ -0,0 +1,128 @@
+USE lower_customer_satisfaction_analysis;
+
+DESC Order_items;
+DESC Orders;
+DESC Product;
+DESC Review;
+
+ALTER TABLE Orders
+ADD COLUMN delivery_category Varchar(40);
+
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE Orders
+SET delivery_category = 
+	CASE 
+		WHEN order_delivered_customer_date > order_estimated_delivery_date THEN 'Late'
+		ELSE 'On-time'
+	END
+WHERE order_delivered_customer_date IS NOT NULL 
+AND order_estimated_delivery_date IS NOT NULL;
+
+SET SQL_SAFE_UPDATES = 1;
+
+ALTER TABLE Orders
+DROP COLUMN delivery_category;
+
+# for this analysis, we use review score as a proxy of customer satisfaction
+
+# Hypothesis 1 : Late Delivery may leads lower customer satisfaction
+
+WITH DeliveryReview AS (
+	SELECT
+		o.delivery_category,
+        r.review_score
+	FROM Orders o
+    JOIN Review r
+    ON o.order_id = r.order_id
+    WHERE delivery_category IS NOT NULL
+)
+
+SELECT 
+	delivery_category,
+    ROUND(AVG(review_score), 2) AS Avg_review
+FROM DeliveryReview
+GROUP BY delivery_category;
+
+# Finding : I compared Average Review Score between Late and On-time delivery status. I noticed that On-time delivery has higher avg score (4.29) than Late delivery (2.57).
+
+# Interpretation : The result support my hypothesis that Late delivery may contribute to lower customer satisfaction.
+
+# Hypothesis 2: Higher freight cost may increase the lower customer satisfaction
+
+WITH FreightReview AS (
+	SELECT
+		oi.freight_category,
+        r.review_score
+	FROM Order_items oi
+    JOIN Review r
+    ON oi.order_id = r.order_id
+    WHERE freight_category IS NOT NULL
+) 
+
+SELECT
+	freight_category,
+    ROUND(AVG(review_score), 2) AS Avg_review
+FROM FreightReview
+GROUP BY freight_category;
+
+# FINDING: I compared average review score across all freight category. I noticed that Average review score decreased from 4.11 for low freight cost to 4.04 for medium freight cost and 3.94 for high freight cost
+
+# Interpretation : This result support my hypothesis that higher freight cost may contribute to the lower customer satisfaction
+
+# Hypothesis 3 : Higher product price may contribute to lower customer satisfaction
+
+WITH PriceReview AS (
+	SELECT
+		oi.price_category,
+        r.review_score
+	FROM Order_items oi
+    JOIN Review r
+    ON oi.order_id = r.order_id
+    WHERE price_category IS NOT NULL
+) 
+
+SELECT
+	price_category,
+    ROUND(AVG(review_score), 2) AS Avg_review
+FROM PriceReview
+GROUP BY price_category;
+
+# FINDING: I compared average review score across all price category. I noticed that average review score is relatively same across all the price category (4.03).
+
+# Interpretation : This result doesnot support my hypothesis which means Higher product price might not contributed to lower customer satisfaction
+
+# Hypothesis 4: longer seller handling time may contribute to lower customer satisfaction?
+
+WITH SellerHandlingReview AS (
+	SELECT
+		o.handling_category,
+        r.review_score
+	FROM Orders o
+    JOIN Review r
+    ON o.order_id = r.order_id
+    WHERE handling_category IS NOT NULL
+)
+
+SELECT 
+	handling_category,
+    ROUND(AVG(review_score), 2) AS Avg_review
+FROM SellerHandlingReview
+GROUP BY handling_category;
+
+#Findings : The seller taking too much time to deliver got lowest avg review score (3.69) while sellers who takes less time to deliver got highest avg score (4.26)
+    
+# Interpretation : This result support my hypothesis which means, longer seller handling time may contribute to lower customer satisfaction.
+
+/* BUSINESS RECOMMENDATION THROUGH MY HYPOTHESIS 
+
+1. Focus more on delivery time and reduce late deliveries.
+   Late delivery has a lower average review score, so the company should focus on improving delivery time and reducing late deliveries.
+
+2. Optimize shipping costs.
+   Higher freight cost is associated with lower average review scores, so the company should look for ways to optimize shipping costs.
+
+3. Reduce seller processing time.
+   Orders with longer seller handling time have lower average review scores, so sellers should try to process and hand over orders to the carrier faster.
+
+*/
