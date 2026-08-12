f0f48b1aefd8efa5d020e7b310934bcf2e518015
Rista Khadka <ristakhadka@Ristas-MacBook-Pro.local>
2026-08-12 14:50:25 +0545

    Added new hypothisis and analyzed

--- sql/Queries_and_analysis.sql
+++ sql/Queries_and_analysis.sql
@@ -4,6 +4,7 @@ DESC Order_items;
 DESC Orders;
 DESC Product;
 DESC Review;
+DESC Customers;
 
 ALTER TABLE Orders
 ADD COLUMN delivery_category Varchar(40);
@@ -45,6 +46,7 @@ GROUP BY delivery_category;
 
 # Interpretation : The result support my hypothesis that Late delivery may contribute to lower customer satisfaction.
 
+-- -------------------------------------------------------------------------------------------------------------------------------------------
 # Hypothesis 2: Higher freight cost may increase the lower customer satisfaction
 
 WITH FreightReview AS (
@@ -67,6 +69,7 @@ GROUP BY freight_category;
 
 # Interpretation : This result support my hypothesis that higher freight cost may contribute to the lower customer satisfaction
 
+-- -------------------------------------------------------------------------------------------------------------------------------------------------
 # Hypothesis 3 : Higher product price may contribute to lower customer satisfaction
 
 WITH PriceReview AS (
@@ -89,6 +92,7 @@ GROUP BY price_category;
 
 # Interpretation : This result doesnot support my hypothesis which means Higher product price might not contributed to lower customer satisfaction
 
+-- --------------------------------------------------------------------------------------------------------------------------------------------
 # Hypothesis 4: longer seller handling time may contribute to lower customer satisfaction?
 
 WITH SellerHandlingReview AS (
@@ -111,15 +115,105 @@ GROUP BY handling_category;
     
 # Interpretation : This result support my hypothesis which means, longer seller handling time may contribute to lower customer satisfaction.
 
+-- --------------------------------------------------------------------------------------------------------------------------
+# Hypothesis 5: Extreme delivery delays are mainly caused by the seller or the carrier.
+
+WITH DeliveryTime AS (
+    SELECT
+        order_id,
+        DATEDIFF(
+            order_delivered_customer_date,
+            order_purchase_timestamp
+        ) AS total_delivery_days,
+        DATEDIFF(
+            order_delivered_carrier_date,
+            order_approved_at
+        ) AS seller_handling_days,
+        DATEDIFF(
+            order_delivered_customer_date,
+            order_delivered_carrier_date
+        ) AS shipping_days
+    FROM Orders
+    WHERE order_purchase_timestamp IS NOT NULL
+      AND order_approved_at IS NOT NULL
+      AND order_delivered_carrier_date IS NOT NULL
+      AND order_delivered_customer_date IS NOT NULL
+),
+
+Ranked AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (
+            ORDER BY total_delivery_days DESC
+        ) AS rn,
+        COUNT(*) OVER () AS total_orders
+    FROM DeliveryTime
+),
+
+ExtremeDelays AS (
+    SELECT *
+    FROM Ranked
+    WHERE rn <= CEIL(total_orders * 0.01)
+)
+
+SELECT
+    ROUND(AVG(seller_handling_days), 2) AS AvgSellerHandling,
+    ROUND(AVG(shipping_days), 2) AS AvgCarrierShipping,
+    ROUND(
+        AVG(seller_handling_days) /
+        (AVG(seller_handling_days) + AVG(shipping_days)) * 100,
+        2
+    ) AS SellerContributionPct,
+    ROUND(
+        AVG(shipping_days) /
+        (AVG(seller_handling_days) + AVG(shipping_days)) * 100,
+        2
+    ) AS CarrierContributionPct
+FROM ExtremeDelays;
+
+#Finding: I analyzed the top 1% of orders with the longest delivery times.I noticed that the average carrier shipping time was 53.31 days, while seller handling took only 8.58 days.
+
+#Interpretation: This suggests that the carrier/shipping stage is the main bottleneck in extreme delivery delays, accounting for approximately 86% of the total average processing time.
+
+# Hypothesis 6: Which geographic area has the highest average delivery time?
+
+SELECT
+    c.customer_state,
+    ROUND(
+        AVG(
+            DATEDIFF(
+                o.order_delivered_customer_date,
+                o.order_purchase_timestamp
+            )
+        ), 2
+    ) AS AvgDeliveryDays
+FROM Orders o
+JOIN Customers c
+    ON o.customer_id = c.customer_id
+WHERE o.order_purchase_timestamp IS NOT NULL
+  AND o.order_delivered_customer_date IS NOT NULL
+GROUP BY c.customer_state
+ORDER BY AvgDeliveryDays DESC;
+
+#Finding: I compared average delivery time across different states.I noticed:that ES state has the highest average delivery time of 29.34 days.
+
+#Interpretation: This suggests that customers in ES stae area may experience longer delivery times, possibly due to distance or logistics constraints.
+-- --------------------------------------------------------------------------------------------------------------------------------------------
 /* BUSINESS RECOMMENDATION THROUGH MY HYPOTHESIS 
 
 1. Focus more on delivery time and reduce late deliveries.
-   Late delivery has a lower average review score, so the company should focus on improving delivery time and reducing late deliveries.
+	Late delivery has a lower average review score, so the company should focus on improving delivery time and reducing late deliveries.
 
 2. Optimize shipping costs.
-   Higher freight cost is associated with lower average review scores, so the company should look for ways to optimize shipping costs.
+	Higher freight cost is associated with lower average review scores, so the company should look for ways to optimize shipping costs.
 
 3. Reduce seller processing time.
-   Orders with longer seller handling time have lower average review scores, so sellers should try to process and hand over orders to the carrier faster.
+	Orders with longer seller handling time have lower average review scores, so sellers should try to process and hand over orders to the carrier faster.
+
+4. Improve carrier performance
+	Carrier shipping takes much longer than seller handling in extreme-delay orders, so the company should focus on reducing the time orders spend in the shipping stage.
+
+5. Investigate high-delay geographic areas
+	The company should focus on areas with longer delivery times and find ways to reduce delivery delays.
 
 */
